25f1f67c325b6db1ff180e019e1edfd1583fd5b3
Ali Burak Pekışık <burakpekisik@gmail.com>
2024-02-21 21:33:27 +0300

    Project created on Git

---
+++ core/views.py
@@ -0,0 +1,87 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from core.models import GeneralSetting, ImageSetting, Skill, Experience, Education, SocialMedia, Document
+
+
+# Create your views here.
+
+def get_general_setting(parameter):
+    try:
+        obj = GeneralSetting.objects.get(name=parameter).parameter
+    except:
+        obj = ''
+
+    return obj
+
+
+def get_image_setting(parameter):
+    try:
+        obj = ImageSetting.objects.get(name=parameter).file
+    except:
+        obj = ''
+
+    return obj
+
+
+def layout(request):
+    site_title = get_general_setting('site_title')
+    site_keywords = get_general_setting('site_keywords')
+    site_description = get_general_setting('site_description')
+    home_banner_name = get_general_setting('home_banner_name')
+    home_banner_title = get_general_setting('home_banner_title')
+    home_banner_description = get_general_setting('home_banner_description')
+    about_myself_welcome = get_general_setting('about_myself_welcome')
+    about_myself_footer = get_general_setting('about_myself_footer')
+    birth_date = get_general_setting('birth_date')
+    phone_number = get_general_setting('phone_number')
+    email = get_general_setting('email')
+    location = get_general_setting('location')
+
+    # Images
+    header_logo = get_image_setting('header_logo')
+    home_banner_image = get_image_setting('home_banner_image')
+    site_favicon = get_image_setting('site_favicon')
+
+    social_medias = SocialMedia.objects.all()
+    documents = Document.objects.all()
+
+    context = {
+        'site_title': site_title,
+        'site_keywords': site_keywords,
+        'site_description': site_description,
+        'home_banner_name': home_banner_name,
+        'home_banner_title': home_banner_title,
+        'home_banner_description': home_banner_description,
+        'about_myself_welcome': about_myself_welcome,
+        'about_myself_footer': about_myself_footer,
+        'birth_date': birth_date,
+        'phone_number': phone_number,
+        'email': email,
+        'location': location,
+        'header_logo': header_logo,
+        'home_banner_image': home_banner_image,
+        'site_favicon': site_favicon,
+        'documents': documents,
+        'social_medias': social_medias,
+    }
+    return context
+
+
+def index(request):
+    # Skills
+    skills = Skill.objects.all()
+
+    experiences = Experience.objects.all().order_by('-start_date')
+    educations = Education.objects.all().order_by('-start_date')
+
+    context = {
+        'skills': skills,
+        'experiences': experiences,
+        'educations': educations,
+    }
+
+    return render(request, 'index.html', context=context)
+
+
+def redirect_urls(request, slug):
+    doc = get_object_or_404(Document, slug=slug)
+    return redirect(doc.file.url)
