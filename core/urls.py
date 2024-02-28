878e3309778acad7b6cbcacd9b32d4203d311cb0
Ali Burak Pekışık <burakpekisik@gmail.com>
2024-02-28 14:35:06 +0300

    refactor: urls updated

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,15 @@
+from django.template.defaulttags import url
 from django.urls import path
+
+from resume import settings
 from .views import index, redirect_urls
+from django.views.static import serve
 
 urlpatterns = [
     path('', index, name='index'),
-    path('<slug>/', redirect_urls, name='redirect_urls')
+    path('<slug>/', redirect_urls, name='redirect_urls'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
+
+
